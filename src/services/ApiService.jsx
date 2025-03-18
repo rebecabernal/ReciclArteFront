8ab33e94f307428467fba1f2221dedd27139ebb8
cuyssi <anabcastrolopez@gmail.com>
2025-03-18 21:44:37 +0100

    feat: service

--- src/services/ApiService.jsx
+++ src/services/ApiService.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import axios from 'axios'
 
-const ApiService = () => {
-  return (
-    <div>ApiService</div>
-  )
+const API_URL= ' ';
+  
+export async function getData() {
+    try {
+        const response = await axios.get(API_URL)
+        return response.data
+    } catch (error) {
+        console.error('Error fetching data: ', error)
+    }   
 }
 
-export default ApiService
+export async function postData(data) {
+    try {
+        const response = await axios.post(API_URL, data, {
+            headers: {
+                'Content-Type': 'application/json',                   
+            }
+        });
+        return response.data
+    } catch (error) {
+        console.error('Error posting data: ', error)
+    }
+}
+
