73009373e53f1b2d2eb67bee811b2b733ac9fd7e
Priscila Oliveira <pguillen.oliv@gmail.com>
2025-03-18 19:50:16 +0100

    creates structure of login and register pages, changes mainbutton css

---
+++ src/pages/registerpage/RegisterPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './RegisterPage.css'
+
+function RegisterPage () {
+
+  return (
+      <>
+    <main className='main-section'>
+      <h2>Bienvenido/a/e</h2>
+      <h3>Registro</h3>
+      <section className='form-container'>
+    {/* llamar el componente FORMULARIO DE REGISTRO*/}
+      </section>
+    </main>
+      </>
+  )
+}
+
+export default RegisterPage
