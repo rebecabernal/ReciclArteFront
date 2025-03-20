fb79e354873cf4b42375d61ff23e5c8cf1f9f8ea
Priscila Oliveira <pguillen.oliv@gmail.com>
2025-03-20 09:46:35 +0100

    adds missing pages

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -6,7 +6,7 @@ function HomePage () {
   return (
       <>
     <main className='main-section'>
-      <h3>Productos en tu zona</h3>
+      <h3>Articulos en tu zona</h3>
       <section className='products-container'>
     {/* llamar el componente PRODUCT CARD */}
       </section>
---
+++ src/pages/product-register/ProductRegister.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './ProductRegister.css'
+
+function ProdRegisterPage () {
+
+  return (
+    <>
+    <main className='main-section'>
+      <h3>Registra tu nuevo producto</h3>
+      <section className='main-section'>
+    {/* llamar el componente FORMULARIO DE LOGIN*/}
+      </section>
+    </main>
+      </>
+  )
+}
+
+export default ProdRegisterPage
