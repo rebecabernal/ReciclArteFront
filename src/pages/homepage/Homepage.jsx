bbe9752f536bf12df187d66068ae81666de3773e
cuyssi <anabcastrolopez@gmail.com>
2025-03-20 21:31:32 +0100

    change styles

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -6,7 +6,7 @@ function HomePage () {
   return (
       <>
     <main className='main-section'>
-      <h3>Articulos en tu zona</h3>
+      <h3 className='home_h3'>Articulos disponibles:</h3>
       <section className='products-container'>
     {/* llamar el componente PRODUCT CARD */}
       </section>
--- src/pages/product-register/ProductRegister.jsx
+++ src/pages/product-register/ProductRegister.jsx
@@ -6,7 +6,7 @@ function ProdRegisterPage () {
   return (
     <>
     <main className='main-section'>
-      <h3>Registra tu nuevo producto</h3>
+      <h3 className='productregister_h3'>Registra tu nuevo producto</h3>
       <section className='main-section'>
     {/* llamar el componente FORMULARIO DE LOGIN*/}
       </section>
